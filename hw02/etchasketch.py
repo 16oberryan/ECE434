eaf39f8b9febdce0aae6b66d80df91052c3cc51b
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-09-16 17:34:48 -0400

    etchasketch.py is works with buttons (no clear button)

--- hw02/etchasketch.py
+++ hw02/etchasketch.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-import curses
-from curses import wrapper
 import time
 import Adafruit_BBIO.GPIO as GPIO
 
@@ -15,32 +13,34 @@ GPIO.setup(but1, GPIO.IN)
 GPIO.setup(but2, GPIO.IN)
 GPIO.setup(but3, GPIO.IN)
 
+gridSize = 8
+lines = 0
+cols = 0
+
+if (len(sys.argv) >= 2):
+	gridSize = int(sys.argv[1])
+
 coordx = 1
 coordy = 1
-pad = 0
+screen = [[None for x in range(gridSize+1)] for y in range(gridSize+1)]
 
-def resetScreen(pad):
-	pad.addch(0,0,' ')
-	stdscr.refresh()
-	n=0
-	while (n<gridSize):
-		pad.addch(n+48)
-		n=n+1
-	n=0
-	while (n<gridSize):
-		pad.addch(n+1,0,n+48)
-		n=n+1
-	pad.refresh(0,0, 0,0, lines, cols)
-	pad.move(coordy, coordx)
+def resetScreen():
+	i = 0
+	print("")
+	while (i<=gridSize):
+		j = 0
+		while(j<=gridSize):
+			print(screen[i][j], end='')
+			j+=1
+		print("")
+		i+=1
 
 def detectedInput(channel):
 	global coordy
 	global coordx
-	global pad
 	newY = coordy
 	newX = coordx
 	if channel == but0:
-		time.sleep(1)
 		newX = coordx+1
 		if newX == gridSize+1:
 			newX = 1
@@ -56,50 +56,30 @@ def detectedInput(channel):
 		newY = coordy+1
 		if newY == gridSize+1:
 			newY = 1
-	try:
-		pad.addch(newY,newX,'X')
-	except curses.error as e:
-		pass
-	pad.move(newY,newX)
+	screen[newX][newY] = 'X'
 	coordy = newY
 	coordx = newX
-	pad.refresh(0,0, 0,0, lines,cols)
+	resetScreen()
 
-def main(stdscr):
-	global pad
-	# Clear screen
-	stdscr.clear()
-	pad = curses.newpad(int(gridSize)+1, int(gridSize)+1)
-	lines = gridSize+1
-	if (curses.LINES-1 < lines):
-		lines = curses.LINES-1
-	cols = gridSize+1
-	if (curses.COLS-1 < cols):
-		cols = curses.COLS-1
-	resetScreen(pad)
-	pad.move(1,1)
-	pad.refresh(0,0, 0,0, lines,cols)
-	#curs_set(False)
-	#coordy = 1
-	#coordx = 1
-	c = 0
-	GPIO.add_event_detect(but0, GPIO.RISING, callback=detectedInput)
-	while True:
-		c = stdscr.getch()
-		if (c == ord(' ')):
-			pad.clear()
-			resetScreen(pad)
-		if (c == ord('q')):
-			break
-
-gridSize = 8
-lines = 0
-cols = 0
-
-if (len(sys.argv) >= 2):
-	gridSize = int(sys.argv[1])
-print("Etch A Sketch:\nRun './etchasketch.py X' to run with custom size 'X' (default is 8).\nTo Play: Use arrow keys to move and 'space' to clear.\nPress 'q' to quit.")
-time.sleep(7)
-stdscr = curses.initscr()
-wrapper(main)
-curses.endwin()
+print("Etch A Sketch:\nRun './etchasketch.py X' to run with custom size 'X' (default is 8).\nTo Play: Use buttons to move and be sad if you want to clear.\nPress ctrl+C to quit.\n(It may help to shrink your terminal window to the size of the board)")
+time.sleep(2)
+screen[0][0] = ' '
+n = 0
+while (n < gridSize):
+	screen[n+1][0] = n
+	screen[0][n+1] = n
+	n+=1
+i = 1
+while (i <= gridSize):
+	j = 1
+	while (j <= gridSize):
+		screen[i][j] = ' '
+		j+=1
+	i+=1
+resetScreen()
+GPIO.add_event_detect(but0, GPIO.RISING, callback=detectedInput)
+GPIO.add_event_detect(but1, GPIO.RISING, callback=detectedInput)
+GPIO.add_event_detect(but2, GPIO.RISING, callback=detectedInput)
+GPIO.add_event_detect(but3, GPIO.RISING, callback=detectedInput)
+while True:
+	pass
